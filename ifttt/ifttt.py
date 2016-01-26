1ccc3a26893c3bd844ad050ec39f701b02f55fae
Oliver Steele <steele@osteele.com>
2016-01-26 16:18:21 -0500

    move config from config file -> stage variables

--- ifttt/ifttt.py
+++ ifttt/ifttt.py
@@ -1,9 +1,7 @@
-from publish_message import publish
 import logging
-from config import config
+from publish_message import publish, configure
 
 API_VERSION = '7'
-IFTTT_TOKEN = config['IFTTT_TOKEN']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,11 +11,15 @@ def handler(event, context):
     logger.info('Version %s', API_VERSION)
     logger.info('Received event: {}'.format(event))
 
-    token = event.get('token')
-    if token != IFTTT_TOKEN:
+    ifttt_token = event['env']['IFTTT_TOKEN']
+    mqtt_url = event['env']['MQTT_URL']
+
+    token = event['token']
+    if token != ifttt_token:
         logger.error("Request token (%s) does not match expected", token)
         raise Exception("Invalid request token")
 
     action = event['action']
+    configure(mqtt_url)
     publish('action', action=action)
     return dict(version=API_VERSION, action=action)
