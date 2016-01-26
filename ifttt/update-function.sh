e4288a1ba6981436804135f0619d109b18f5c551
Oliver Steele <steele@osteele.com>
2016-01-26 09:09:59 -0500

    don't upload *.pyc

--- ifttt/update-function.sh
+++ ifttt/update-function.sh
@@ -4,7 +4,8 @@ FUNCTION_NAME=highland-ifttt
 ZIPFILE=ifttt.zip
 
 rm -f $ZIPFILE
-zip -r $ZIPFILE *.py *.json paho
+zip -r $ZIPFILE *.py *.json
+find paho ! -name \*.pyc -print | xargs zip ifttt.zip
 
 aws lambda update-function-code \
   --function-name $FUNCTION_NAME \
